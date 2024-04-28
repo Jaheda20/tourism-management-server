336a3ebc592cf855b8c422307ae9c6b4fb719be2
Jaheda20 <jahedasultana20@gmail.com>
2024-04-28 09:10:02 +0200

    created api for individual spot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -50,7 +50,14 @@ async function run() {
     app.get('/addSpots', async(req, res) =>{
         const cursor = spotCollection.find();
         const spots = await cursor.toArray();
-        res.send(spots);
+        res.send(spots)
+    })
+
+    app.get('/addSpots/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await spotCollection.findOne(query)
+        res.send(result)
     })
     
     app.post('/addSpots', async(req, res) => {
